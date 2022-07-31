256b5e710acf36b16940a36df895f3a0e0f557eb
Tenghy23 <66905172+Tenghy23@users.noreply.github.com>
2022-07-31 15:20:18 +0800

    added tools

---
+++ Fabric-Intro-2/src/main/java/net/fabricmc/learnfabric/item/custom/ModAxeItem.java
@@ -0,0 +1,80 @@
+package net.fabricmc.learnfabric.item.custom;
+
+import net.fabricmc.learnfabric.util.ModTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import java.util.List;
+
+// when creating an advanced item, extend the Item and make your own class name
+// a dowsing rod is an item to detect if valuable ore is located below object
+public class DowsingRodItem extends Item {
+    public DowsingRodItem(Settings settings) {
+        super(settings);
+    }
+
+    // then override the methods inside Item class for custom bfehaviour
+    // useOnBlock occurs when right click on block
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        // wrap in this if statement to ensure you are in client
+        if(context.getWorld().isClient) {
+            BlockPos positionClicked = context.getBlockPos();
+            PlayerEntity player = context.getPlayer();
+            boolean foundBlock = false;
+
+            // loop from top pos all the way down, identify valuable block and return pos
+            for(int i = 0; i <= positionClicked.getY(); i++) {
+                BlockState stateBelow = context.getWorld().getBlockState(positionClicked.down(i));
+                Block blockBelow = stateBelow.getBlock();
+
+                if(isValuableBlock(stateBelow)) {
+                    outputValuableCoordinates(positionClicked.add(0, -i, 0), player, blockBelow);
+                    foundBlock = true;
+                    break;
+                }
+            }
+
+            // return custom message when unable to find valuable block
+            if(!foundBlock) {
+                player.sendMessage(Text.translatable("item.learnfabric.dowsing_rod.no_valuables"), false);
+            }
+        }
+
+        // apply 1 dmg to the item
+        // break the item on the player's hand once 0 durability is reached
+        context.getStack().damage(1, context.getPlayer(),
+                (player) -> player.sendToolBreakStatus(player.getActiveHand()));
+
+        return super.useOnBlock(context);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("item.learnfabric.dowsing_rod.tooltip.shift"));
+        } else {
+            tooltip.add(Text.translatable("item.learnfabric.dowsing_rod.tooltip"));
+        }
+    }
+
+    private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block blockBelow) {
+        player.sendMessage(Text.literal("Found " + blockBelow.asItem().getName().getString() + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"), false);
+    }
+
+    private boolean isValuableBlock(BlockState block) {
+        return block.isIn(ModTags.Blocks.DOWSING_ROD_DETECTABLE_BLOCKS);
+    }
+}
---
+++ Fabric-Intro-2/src/main/java/net/fabricmc/learnfabric/item/custom/ModPaxelItem.java
@@ -0,0 +1,58 @@
+package net.fabricmc.learnfabric.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.learnfabric.item.custom.DowsingRodItem;
+import net.fabricmc.learnfabric.item.custom.ModAxeItem;
+import net.fabricmc.learnfabric.item.custom.ModHoeItem;
+import net.fabricmc.learnfabric.item.custom.ModPickaxeItem;
+import net.minecraft.item.*;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.fabricmc.learnfabric.MCCourseMod;
+
+public class ModItems {
+
+    // 3. making a new item: ORICHALCUM_IGNOT, name has to be lowercase
+    // for every new item, add tne static final Item
+    public static final Item ORICHALCUM_INGOT = registerItem("orichalcum_ingot",
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_NUGGET = registerItem("orichalcum_nugget",
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item RAW_ORICHALCUM = registerItem("raw_orichalcum",
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    // maxDamage here refers to total durability value
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroups.COURSE).maxDamage(32)));
+    public static final Item TURNIP = registerItem("turnip",
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE).food(ModFoodComponents.TURNIP)));
+    public static final Item COAL_SLIVER = registerItem("coal_silver",
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    public static final Item ORICHALCUM_PICKAXE = registerItem("orichalcum_pickaxe",
+            new ModPickaxeItem(ModToolMaterial.ORICHALCUM, 1, 2f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_AXE = registerItem("orichalcum_axe",
+            new ModAxeItem(ModToolMaterial.ORICHALCUM, 3, 1f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_HOE = registerItem("orichalcum_hoe",
+            new ModHoeItem(ModToolMaterial.ORICHALCUM, 0, 0f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_SHOVEL = registerItem("orichalcum_shovel",
+            new ShovelItem(ModToolMaterial.ORICHALCUM, 1, 2f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+    public static final Item ORICHALCUM_SWORD = registerItem("orichalcum_sword",
+            new SwordItem(ModToolMaterial.ORICHALCUM, 3, 3f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    private static Item registerItem(String name, Item item) {
+        // 2. setup base method to register mod
+        return Registry.register(Registry.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        // 1. setup to identify mod is successfully registered in console
+        // 1a. setup gradle.properties + fabric.mod.json + learnfabric.mixins.json
+        System.out.println("Register Mod Items for " + MCCourseMod.MOD_ID);
+    }
+}
